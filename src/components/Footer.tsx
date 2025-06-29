443f72e1e91d73abd72521581b66b22fe7bbbfaa
bruce-waynee <muhamedalimousa03@gmail.com>
2025-06-29 12:43:57 +0200

    * update font family

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
       <footer className="mt-8 py-8">
-        <div className="container max-w-2xl mx-auto flex justify-between items-center text-sm text-muted-foreground">
+        <div className="container max-w-2xl mx-auto flex justify-between items-center text-sm text-muted-foreground font-mono">
           <span>Muhamad Ali Mousa</span>
           <a 
             href="https://github.com/bruce-waynee/about-me" 
