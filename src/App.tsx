c34a07b7037f0d1b4491567fe294e34eb97fdaf9
bruce-waynee <muhamedalimousa03@gmail.com>
2025-06-29 11:37:03 +0200

    * fix path

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <BrowserRouter>
         <Routes>
-          <Route path="/about" element={<Index />} />
+          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
