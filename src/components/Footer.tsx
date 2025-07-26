e778eeca44ee93811a9439a74993574d5403181d
bruce-waynee <muhamedalimousa03@gmail.com>
2025-07-26 21:04:10 +0200

    *remove source link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ export function Footer() {
       <footer className="mt-8 py-8">
         <div className="container max-w-2xl mx-auto flex justify-between items-center text-sm text-muted-foreground font-mono">
           <span>Muhamad Ali Mousa</span>
-          <a 
+          {/* <a 
             className="hover:text-foreground transition-colors underline"
             href="https://github.com/bruce-waynee/about-me" 
             target="_blank"
           >
             Source
-          </a>
+          </a> */}
         </div>
       </footer>
     );
